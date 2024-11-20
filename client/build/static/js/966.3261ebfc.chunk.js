96f92149bff334c31ff321796f8c897ba150868a
vidhvath28 <vidhvathj.2005@gmail.com>
2024-11-20 10:35:20 +0530

    new

---
+++ client/build/static/js/966.3261ebfc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[966],{966:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=966.3261ebfc.chunk.js.map
